6f80aa2663a034575ace01cc19c12b5e446de45c
BladeOfFreedom <ardaplnl.5@gmail.com>
2026-03-01 15:54:07 +0300

    Add initial implementation of a simple lexer and parser for a mini language

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        File file = null;
+        File file;
         try {
             file = new File("input.txt");
             Scanner inp = new Scanner(file);
-            String inputString = "";
+            StringBuilder inputString = new StringBuilder();
             while(inp.hasNextLine()){
-                inputString += inp.nextLine() + " ";
+                inputString.append(inp.nextLine()).append(" ");
             }
             //System.out.println(inputString);
 
-            List<Token> tokens = Lexer.tokenize(inputString);
+            List<Token> tokens = Lexer.tokenize(inputString.toString());
             Parser parser = new Parser(tokens);
             List<Statement> parsedStatements = parser.parse();
 
